a060ae8835b0d146af0746ae616b82b9ea215f57
Eli-2009 <elieliyarov18@gmail.com>
2024-10-19 19:06:10 +0400

    homework

---
+++ app.js
@@ -0,0 +1,67 @@
+// let btn = document.querySelectorAll('button')
+
+
+
+// btn.forEach(btn =>{
+//     btn.addEventListener('click', () =>{
+//         if (btn.style.color != 'rgb(139, 137, 137)') {
+//             btn.style.color = 'rgb(139, 137, 137)'
+//         }
+//         else{
+//             btn.style.color = 'black'
+//         }
+//     })
+// })
+
+
+
+
+
+// let btn = document.querySelectorAll('button')
+// let number = 0
+
+
+// btn.forEach(btn =>{
+//     btn.addEventListener('click', () =>{
+//         number +=1;
+//         if (number === 1) {
+//             btn.style.color = 'blue'
+//         }else if(number === 2) {
+//             btn.style.color = 'black'
+//         }else{
+//             btn.style.color = 'black'
+//         }
+
+//         btn.classList.toggle('decoration')
+//     })
+// })
+
+
+// document.addEventListener('keypress', (e) =>{
+//     if (e.key === 'Enter') {
+//         console.log('Enter');
+//     }
+// })
+
+
+// let btn = document.querySelector('.button-class')
+// let input = document.querySelector('input')
+// btn.addEventListener('click', () =>{
+//     console.log(input.value);
+// })
+
+
+
+
+
+// let btn = document.querySelector('.send-button')
+// let input = document.querySelector('.num-class')
+
+
+// btn.addEventListener('click', () => {
+//     if (input.value.length > 5) {
+//         console.log('girise icaze verildi');
+//     }else{
+//         console.log('girise icaze verilmedi');
+//     }
+// })
